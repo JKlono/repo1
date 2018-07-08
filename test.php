4cdd1bfa463e0409e634571ae870df82eac0f424
JKlono <joek137@gmail.com>
2018-07-07 23:14:02 -0400

    First commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = "joe";
+$pass = "mypassworfd";
+$conn = new PDO("mysql:host=localhost;dbname=test", $user, $pass);
+//$conn = new PDO("mysql:host=localhost;dbname=test");
+
+//$sth = $conn->prepare("SELECT * FROM mytest WHERE first = 'kristi'");
+//$sth->execute();
+//$result = $sth->fetchAll();
+//$account = $result['0'];
+
+echo "Hello.  Here is the array: <br/> <br/>";
+print_r($account);
+?>
