f4ce26667467345cbeba58bc89eb910a4e0d8669
JKlono <joek137@gmail.com>
2018-07-09 23:36:17 -0400

    testing

--- web/test.php
+++ web/test.php
@@ -2,7 +2,7 @@
 //Test script
 $user = "joe";
 $pass = "mypassworfd";
-$conn = new PDO("mysql:host=localhost;dbname=test", $user, $pass);
+//$conn = new PDO("mysql:host=localhost;dbname=test", $user, $pass);
 //$conn = new PDO("mysql:host=localhost;dbname=test");
 
 //$sth = $conn->prepare("SELECT * FROM mytest WHERE first = 'kristi'");
@@ -11,5 +11,5 @@ $conn = new PDO("mysql:host=localhost;dbname=test", $user, $pass);
 //$account = $result['0'];
 
 echo "Hello.  Here is the array: <br/> <br/>";
-print_r($account);
+//print_r($account);
 ?>
