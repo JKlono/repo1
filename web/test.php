f8f9cb6eba62d3390325e30992e2ca6bb17695e9
JKlono <joek137@gmail.com>
2018-07-09 23:44:39 -0400

    Connect to DB attempot 2

--- web/test.php
+++ web/test.php
@@ -5,7 +5,7 @@ $pass = "v8svds1zt43sfz47";
 $conn = new PDO("mysql:host=ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=msglfa18mc2ig9k3", $user, $pass);
 //$conn = new PDO("mysql:host=localhost;dbname=test");
 
-$sth = $conn->prepare("SELECT * FROM mytest WHERE first = 'kristi'");
+$sth = $conn->prepare("SELECT * FROM new_table");
 $sth->execute();
 $result = $sth->fetchAll();
 $account = $result['0'];
