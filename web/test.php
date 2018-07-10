3a7f9d9b99ccde8be20f6e48665c28b46b0b2d1a
JKlono <joek137@gmail.com>
2018-07-09 23:42:29 -0400

    Connect to DB attempot 1

--- web/test.php
+++ web/test.php
@@ -1,15 +1,15 @@
 <?php
 //Test script
-$user = "joe";
-$pass = "mypassworfd";
-//$conn = new PDO("mysql:host=localhost;dbname=test", $user, $pass);
+$user = "nuww995zo3buv717";
+$pass = "v8svds1zt43sfz47";
+$conn = new PDO("mysql:host=ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=msglfa18mc2ig9k3", $user, $pass);
 //$conn = new PDO("mysql:host=localhost;dbname=test");
 
-//$sth = $conn->prepare("SELECT * FROM mytest WHERE first = 'kristi'");
-//$sth->execute();
-//$result = $sth->fetchAll();
-//$account = $result['0'];
+$sth = $conn->prepare("SELECT * FROM mytest WHERE first = 'kristi'");
+$sth->execute();
+$result = $sth->fetchAll();
+$account = $result['0'];
 
 echo "Hello.  Here is the array: <br/> <br/>";
-//print_r($account);
+print_r($account);
 ?>
